c097e1f54aea244f20bdf0ae76c111144d0a0699
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-04-20 15:54:55 +0300

    Add comment post method to post the comments to the API.

--- src/comment.js
+++ src/comment.js
@@ -1,8 +1,33 @@
 const url = 'https://api.tvmaze.com/search/shows?q=simpsons';
+const appKey = 'xyUc6vOROrsRVucrRen9';
+const commentUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appKey}/comments`;
+let dd = 0;
 const commentPopup = document.querySelector('.comment-popup');
-
+let commentCon = document.getElementsByClassName('timeand-comment-cont');
+let commentArray = [];
 export default class Comment {
+  static commentPost = async (comment, id) => {
+    // const myRequest = new Request(commentUrl);
+    await fetch(commentUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(comment),
+    });
+    Comment.commentPopulator(id);
+  }
+
+  static commentCounter = (commens) => {
+    const count = commens.length;
+    return count;
+  }
+
     static commentPopulator = async (ides) => {
+      const commentList = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appKey}/comments?item_id=${ides}`;
+      const rese = await fetch(commentList, {
+        method: 'GET',
+      });
+      const resgett = await rese.json();
+      console.log(resgett);
       const request = new Request(url);
       const response = await fetch(request);
       const bodyResponse = await response.json();
@@ -16,8 +41,28 @@ export default class Comment {
               <h4 class="detail">Premiered
               : ${bodyResponse[i].show.premiered}</h4>
               <h4 class="detail">Score: ${bodyResponse[i].score}</h4>
+          </div>
+          <h2 class="comment-count"></h2>
+          <div class="timeand-comment-cont">
+          </div>
+          <div class="add-comment">
+              <h2 class="com-header">Add a comment</h2>
+              <input type="text" class="userName" placeholder="User Name">
+              <textarea type="text" class="userComment" cols="20" rows="7" placeholder="Enter your message..."></textarea>
+              <input type="submit" class="submitComment" id="${bodyResponse[i].show.id}">
           </div>`;
         }
       }
+      const commentCon = document.querySelector('.timeand-comment-cont');
+      for (let i = 0; i < resgett.length; i += 1) {
+        const div = document.createElement('div');
+        div.classList.add('time-com');
+        div.textContent = `${resgett[i].creation_date}-${resgett[i].username}: ${resgett[i].comment}`;
+        commentCon.appendChild(div);
+      }
+      commentArray = document.querySelectorAll('.time-com');
+      const countBody = document.querySelector('.comment-count');
+      // dd = Comment.commentCounter(commentArray);
+      countBody.textContent = `Comments (${Comment.commentCounter(commentArray)})`;
     }
 }
