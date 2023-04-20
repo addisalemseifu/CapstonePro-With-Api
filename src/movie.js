864b9964cf2c18ddf7a83b5b2e0b279dee98a222
Mohammad Suliman joya <sjoya66@gmail.com>
2023-04-20 16:29:58 +0430

    Update movie file with the like count

--- src/movie.js
+++ src/movie.js
@@ -1,3 +1,5 @@
+import Like from './like.js';
+
 const url = 'https://api.tvmaze.com/search/shows?q=simpsons';
 const movieContaier = document.querySelector('.movieContaier');
 export default class Movie {
@@ -10,20 +12,22 @@ export default class Movie {
 
   static populate = async () => {
     const returnMovies = await Movie.getMovies();
+    const likeArray = await Like.getLikes();
+    let likeCou = 0;
     movieContaier.innerHTML = '';
     for (let i = 0; i < returnMovies.length; i += 1) {
-      // for (let j = 0; j < likeArray.length; j += 1) {
-      //   if (returnMovies[i].show.id === Number(likeArray[j].item_id)) {
-      //     likeCou = likeArray[j].likes;
-      //   }
-      // }
+      for (let j = 0; j < likeArray.length; j += 1) {
+        if (returnMovies[i].show.id === Number(likeArray[j].item_id)) {
+          likeCou = likeArray[j].likes;
+        }
+      }
       movieContaier.innerHTML += `<div class="movie" id="${returnMovies[i].show.id}">
     <img class="picture" src="${returnMovies[i].show.image.original}" alt="">
     <div class="space-container">
         <h3 class="space">${returnMovies[i].show.name}</h3>
         <i class="fa-regular fa-heart"></i>
     </div>
-    <span class="like-count"> Likes</span>
+    <span class="like-count">${likeCou} Likes</span>
     <button class="comments">Comments</button>
 </div>`;
     }
