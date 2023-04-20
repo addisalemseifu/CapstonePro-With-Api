3a8bd1e748eb14bfa4f823a27d321b5b2af8cae5
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-04-20 14:03:51 +0300

    Add an event listner for the comment button.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './index.css';
 import Movie from './movie.js';
+import Comment from './comment.js';
 
+const movieContaier = document.querySelector('.movieContaier');
+
+const commentPopup = document.querySelector('.comment-popup');
+const mainContainer = document.querySelector('.mainContainer');
 document.addEventListener('DOMContentLoaded', () => {
   Movie.populate();
+});
+
+movieContaier.addEventListener('click', (e) => {
+  if (e.target.classList.contains('comments')) {
+    const comentId = e.target.parentElement.id;
+    console.log(comentId)
+    if (commentPopup.classList.contains('active')) {
+      commentPopup.classList.remove('active');
+    }
+    commentPopup.classList.add('active');
+    mainContainer.classList.add('deactive');
+    Comment.commentPopulator(comentId);
+  }
 });
